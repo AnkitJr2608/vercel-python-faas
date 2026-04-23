ae000d8bd5309c55a17af9ea1951c9e34ec7dadf
AnkitJr2608 <99964220+AnkitJr2608@users.noreply.github.com>
2026-04-23 12:08:30 +0530

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -1,5 +1,13 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Hello Ankit 🚀 Python FaaS running on Vercel"
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(json.dumps({
+            "message": "Hello Ankit 🚀",
+            "status": "Python FaaS Working"
+        }).encode())
